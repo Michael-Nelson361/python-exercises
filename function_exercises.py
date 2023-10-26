2daee3a95c38f557796b51d4ea2e0b6f0acc7eaf
Michael Nelson <zsc.michaeln@gmail.com>
2023-10-26 16:55:43 -0500

    Added some docstrings

--- function_exercises.py
+++ function_exercises.py
@@ -13,7 +13,7 @@ def is_two(x):
 
     Parameters
     ----------
-    x : TYPE
+    x : string or int
         Takes any argument.
 
     Returns
@@ -33,10 +33,23 @@ assert is_two(4) == False
 assert is_two('aeiou') == False
 print("Exercise 1 is correct")
 
-
 # Exercise 2
 # Return true if the passed string is a vowel and false otherwise
 def is_vowel(x):
+    """
+    Return if the value passed is a vowel or not.
+
+    Parameters
+    ----------
+    x : string
+        Takes a single-letter string argument.
+
+    Returns
+    -------
+    bool
+        Returns True if the character is a vowel, and False otherwise.
+
+    """
     vowels = ['a','e','i','o','u']
     
     # Check if string has numbers
@@ -57,7 +70,20 @@ print("Exercise 2 is correct")
 # Return True if the string is a consonant, False otherwise
 # Use the is_vowel function to accomplish this
 def is_consonant(x):
-    
+    """
+    Return if the value passed is a consonant or not.
+
+    Parameters
+    ----------
+    x : string
+        Takes a single-letter string argument.
+
+    Returns
+    -------
+    bool
+        Inverses the is_vowel return Returns True if not a vowel, and false otherwise.
+
+    """
     if x.isalpha():
         return not is_vowel(x)
     
@@ -76,6 +102,20 @@ print('Exercise 3 is correct')
 # The function should capitalize the first letter of the word if the word
 # starts with a consonant.
 def the_titler(x):
+    """
+    Returns a word with the first letter capitalized if the word starts with a consonant.
+
+    Parameters
+    ----------
+    x : string
+        Takes a single word string input.
+
+    Returns
+    -------
+    string
+        Titles word if the first letter is a consant. Lowers case of word otherwise.
+
+    """
     if is_consonant(x[0]):
         return x.title()
     else:
@@ -92,6 +132,22 @@ print("Exercise 4 is correct")
 # Define a function to accept a tip percentage, bill total, and
 # return the amount to tip
 def calculate_tip(tip_perc,bill):
+    """
+    Returns the amount to tip based on the bill.
+
+    Parameters
+    ----------
+    tip_perc : float
+        Expects a percentage in decimal format (e.g. 20% would be 0.20).
+    bill : int or float
+        Expects a numerical value that can be either an integer or a float.
+
+    Returns
+    -------
+    float or int
+        Multiplies the two arguments together and returns the result.
+
+    """
     return round(tip_perc * bill,2)
     
 assert calculate_tip(0.2,20) == 4
@@ -102,6 +158,22 @@ assert calculate_tip(0.25,20) == 5
 # Define a function to accept an original price, discount percent, and 
 # returns the prices after discount is applied
 def apply_discount(price,discount):
+    """
+    Returns the discounted price.
+
+    Parameters
+    ----------
+    price : integer or float
+        Expects a numerical value that can be either an integer or float.
+    discount : float
+        Expects a percentage in decimal format (e.g. 20% would be 0.20).
+
+    Returns
+    -------
+    float or int
+        Returns sum of price and price multiplied by discount.
+
+    """
     return round(price-(price*discount),2)
 
 assert apply_discount(50, .10) == 45
@@ -110,7 +182,21 @@ print('Exercise 6 is correct')
 
 # Exercise 7
 def handle_commas(num_string):
-   return int(num_string.replace(',','_'))
+    """
+    Returns a number without any commas in it
+
+    Parameters
+    ----------
+    num_string : string
+        Expects a number formatted as a string.
+
+    Returns
+    -------
+    int
+        Returns the integer of given number (but without any commas in it).
+
+    """
+    return int(num_string.replace(',','_'))
 
 assert handle_commas('1,000') == 1000
 assert handle_commas('1,234,567,890') == 1234567890
@@ -118,6 +204,20 @@ print('Exercise 7 is correct')
 
 # Exercise 8
 def get_letter_grade(grade):
+    """
+    
+
+    Parameters
+    ----------
+    grade : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    str
+        DESCRIPTION.
+
+    """
     if grade >= 90:
         return 'A'
     elif grade >= 80:
@@ -137,6 +237,20 @@ print('Exercise 8 is correct')
 
 # Exercise 9
 def remove_vowels(string):
+    """
+    
+
+    Parameters
+    ----------
+    string : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    string : TYPE
+        DESCRIPTION.
+
+    """
     vowels = ['a','e','i','o','u']
     
     for vowel in vowels:
@@ -155,6 +269,20 @@ print('Exercise 9 is correct')
     
 # Exercise 10
 def normalize_name(string):
+    """
+    
+
+    Parameters
+    ----------
+    string : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    string : TYPE
+        DESCRIPTION.
+
+    """
     # Initialize any variables needed
     lower_alphabet = [chr(ord('a')+i) for i in range(26)]
     
@@ -188,6 +316,20 @@ print('Exercise 10 is correct')
 
 # Exercise 11
 def cumulative_sum(numbers):
+    """
+    
+
+    Parameters
+    ----------
+    numbers : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    new_numbers : TYPE
+        DESCRIPTION.
+
+    """
     curr_sum = 0
     new_numbers = []
     
